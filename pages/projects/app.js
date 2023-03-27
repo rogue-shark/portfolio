9df4452f3b4c633f57a1cc2dd6f338072b666ca8
RogueShark <shantanuss789@gmail.com>
2023-03-27 21:56:06 +0530

    FIX ⛔ : Added main style & app.js file to each page

--- pages/projects/app.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.querySelector('.toggle-button');
-const list = document.querySelector('.nav-list');
-
-toggleButton.addEventListener('click', () => {
-    list.classList.toggle('active')
-})
-
